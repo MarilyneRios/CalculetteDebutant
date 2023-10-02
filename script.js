f838d4b70f44857746aefd5be52fa7ccbc8147c5
Marilyne Rios <rios.marilyne@gmail.com>
2023-10-02 08:18:55 +0200

    fisrt commit

---
+++ script.js
@@ -0,0 +1,33 @@
+/* alert ('calculette pour débutant') */
+
+
+/* DE */
+function deleteLastCharacter() {
+    var currentValue = document.calculatorForm.display.value;
+    document.calculatorForm.display.value = currentValue.slice(0, -1);
+}
+
+/* AC */
+function clearDisplay() {
+    document.calculatorForm.display.value = '';
+}
+
+/* ajouter une valeur à input dans le formulaire calculatorForm */
+function appendToDisplay(value) {
+    document.calculatorForm.display.value += value;
+}
+
+/* try...catch pour évaluer une expression mathématique 
+try = instruction -> bloc de code à exécuter
+catch = instruction -> bloc de code pour gérer une erreur. */
+
+function calculate() {
+    try {
+        /*eval(): prend une chaîne de caractères en tant qu'argument
+         et l'évalue comme une expression mathématique valide  */
+        var result = eval(document.calculatorForm.display.value);
+        document.calculatorForm.display.value = result;
+    } catch (error) {
+        document.calculatorForm.display.value = 'Error';
+    }
+}

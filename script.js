d55b61b90a2e2283ccec6d2e159e74490dfc2717
Marilyne Rios <rios.marilyne@gmail.com>
2023-10-02 08:38:45 +0200

    ajout des explications du code

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-/* alert ('calculette pour débutant') */
+/* toujours les liens entre les fichiers avant de commencer 
+alert ('calculette pour débutant') */
 
 
 /* DE */
 function deleteLastCharacter() {
     var currentValue = document.calculatorForm.display.value;
+    /*methode slice pour découper/retirer la dernière valeur afiichée */
     document.calculatorForm.display.value = currentValue.slice(0, -1);
 }
 
@@ -27,6 +29,8 @@ function calculate() {
          et l'évalue comme une expression mathématique valide  */
         var result = eval(document.calculatorForm.display.value);
         document.calculatorForm.display.value = result;
+        
+        /* Pour afficher une erreur: catch*/
     } catch (error) {
         document.calculatorForm.display.value = 'Error';
     }
